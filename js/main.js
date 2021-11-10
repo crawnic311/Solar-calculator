2f9233e10e877cb6a4ae75f07d994c9195c2e269
Dillon Craw <68667382+crawnic311@users.noreply.github.com>
2021-11-09 17:29:06 -0700

    Finished Functions

--- js/main.js
+++ js/main.js
@@ -1,5 +1,26 @@
 
-function test() {
-alert("Hello World from Me")
-}
+function floor(diameter) {
+    var radius = diameter/2
+    var floorArea = Math.PI * (radius * radius)
+    return floorArea
+} //end of function
+
+function walls(diameter, height) {
+    var radius = diameter/2
+    var circumference = 2 * Math.PI * radius
+    var wallArea = circumference * height
+    return wallArea
+} //end of function 
+
+function materialsNeeded() {
+    var d = document.getElementById("across").value
+    var h = document.getElementById('height').value
+
+    var carpetNeeded = Math.ceil(floor(d))
+    var paintNeeded = Math.ceil(walls(d, h))
+
+    console.log('Carpet needed is '+carpetNeeded+' sqft')
+    console.log('Paint needed is '+paintNeeded+' sqft')
+
+} //end of function
 
